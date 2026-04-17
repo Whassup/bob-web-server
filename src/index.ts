808a7cac45b0d7777d0e518d134059d36320072a
Robert Prib <robert.prib@gmail.com>
2026-04-17 15:45:31 +1000

    Add API html

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { html } from './create-server-path/html.ts';
 import { text } from './create-server-path/text.ts';
 import { createServer } from './create-server/create-server.ts';
 
 const server = createServer()
 
 text(server, "/text", "here is some text")
+html(server, "/html", "<h1>here is some html text</h1>")
